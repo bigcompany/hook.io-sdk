e6b7c2fbc17f1a8ee20bc7cce7c3ee0aa4c5fec5
Marak <marak.squires@gmail.com>
2016-10-01 18:29:32 -0400

    [api] [minor] Use local username when possible   * Better init cancel

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
 var config = {};
 module['exports'] = config;
 
-config.host = "hook.io";
-config.port = 443;
-config.protocol = 'https';
+// dev, david
+
+config.user = "david";
+config.email = "david@marak.com";
+config.host = "localhost";
+config.port = 9999;
+config.protocol = 'http';
+config.accessKey = "dfc2d72c-5044-44c4-afa4-d7a5f322a43b";
