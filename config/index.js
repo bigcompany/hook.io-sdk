ad62f35cd21100ce2b3dd942d0c2b3e77a9be9e2
Marak <marak.squires@gmail.com>
2016-12-24 17:40:36 -0500

    [api] Added ability to set local configuration   * Switch to JSON based configurations   * Can now check access key and set user info   * Update to latest `/services` API

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 var config = {};
-config = require('./dev');
+config = require('./dev/index.json');
+//config = require('./production/index.json');
 module['exports'] = config;
 
-//config = require('./production');
