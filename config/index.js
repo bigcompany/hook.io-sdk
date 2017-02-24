182ec45872d271751bfd7aa745a6ca357e083aef
Marak <marak.squires@gmail.com>
2017-02-24 16:33:14 -0500

    [api] Added the ability to switch config env

--- config/index.js
+++ config/index.js
@@ -1,9 +1,18 @@
 var env = "dev";
 
+var base = require('./index.json');
+
+if (base.configEnv === 'dev') {
+  env = "dev";
+} else {
+  env = "production";
+}
+
 if (process.env.NODE_ENV === "production") {
   env = "production";
 }
-// env = "production";
+
 module.exports = require('./' + env + '.json');
 module.exports.location = require.resolve('./' + env + '.json');
+module.exports.base = base;
 module.exports.env = env;
